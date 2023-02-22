8944c63f1c60e4c546bee39f5ea65955c05087f9
gabo <gabriel.gaboardi13@gmail.com>
2023-02-22 20:23:25 -0300

    updated nav header

--- src/data.js
+++ src/data.js
@@ -1,67 +1,98 @@
 import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 
+// export const headerData = {
+//   links: [
+//     {
+//       text: 'Landing',
+//       links: [
+//         {
+//           text: 'Sass',
+//           href: getPermalink('/landing/saas'),
+//         },
+//         {
+//           text: 'Startup',
+//           href: getPermalink('/landing/startup'),
+//         },
+//         {
+//           text: 'Mobile App',
+//           href: getPermalink('/landing/mobile-app'),
+//         },
+//       ],
+//     },
+//     {
+//       text: 'Pages',
+//       links: [
+//         {
+//           text: 'Features',
+//           href: '#',
+//         },
+//         {
+//           text: 'Pricing',
+//           href: '#',
+//         },
+//         {
+//           text: 'About us',
+//           href: '#',
+//         },
+//         {
+//           text: 'Contact',
+//           href: '#',
+//         },
+//         {
+//           text: 'Terms',
+//           href: getPermalink('/terms'),
+//         },
+//         {
+//           text: 'Privacy policy',
+//           href: getPermalink('/privacy'),
+//         },
+//       ],
+//     },
+//     {
+//       text: 'Widgets',
+//       href: '#',
+//     },
+//     {
+//       text: 'Blog',
+//       href: getBlogPermalink(),
+//     },
+//   ],
+//   actions: [
+//     { type: 'button', text: 'Download', href: 'https://github.com/onwidget/astrowind' }
+//   ],
+// };
+  
 export const headerData = {
   links: [
     {
-      text: 'Landing',
+      text: '/blog',
       links: [
         {
-          text: 'Sass',
-          href: getPermalink('/landing/saas'),
+          text: '/articles',
+          href: getBlogPermalink(),
         },
         {
-          text: 'Startup',
+          text: '/lists',
           href: getPermalink('/landing/startup'),
         },
-        {
-          text: 'Mobile App',
-          href: getPermalink('/landing/mobile-app'),
-        },
       ],
     },
     {
-      text: 'Pages',
-      links: [
-        {
-          text: 'Features',
-          href: '#',
-        },
-        {
-          text: 'Pricing',
-          href: '#',
-        },
-        {
-          text: 'About us',
-          href: '#',
-        },
-        {
-          text: 'Contact',
-          href: '#',
-        },
-        {
-          text: 'Terms',
-          href: getPermalink('/terms'),
-        },
-        {
-          text: 'Privacy policy',
-          href: getPermalink('/privacy'),
-        },
-      ],
-    },
-    {
-      text: 'Widgets',
+      text: '/projects',
       href: '#',
     },
     {
-      text: 'Blog',
+      text: '/snippets',
+      href: getBlogPermalink(),
+    },
+    {
+      text: '/about',
       href: getBlogPermalink(),
     },
   ],
-  actions: [
-    { type: 'button', text: 'Download', href: 'https://github.com/onwidget/astrowind' }
-  ],
-};
   
+};
+
 export const footerData = {
   links: [
     {
