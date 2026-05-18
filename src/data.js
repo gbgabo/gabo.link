aa88b647332e5909bd4da4805845b5114a03c576
gabo <gabriel.gaboardi13@gmail.com>
2026-05-17 23:16:48 -0300

    navigation refactor

--- src/data.js
+++ src/data.js
@@ -1,67 +1,5 @@
 import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 
-// export const headerData = {
-//   links: [
-//     {
-//       text: 'Landing',
-//       links: [
-//         {
-//           text: 'Sass',
-//           href: getPermalink('/landing/saas'),
-//         },
-//         {
-//           text: 'Startup',
-//           href: getPermalink('/landing/startup'),
-//         },
-//         {
-//           text: 'Mobile App',
-//           href: getPermalink('/landing/mobile-app'),
-//         },
-//       ],
-//     },
-//     {
-//       text: 'Pages',
-//       links: [
-//         {
-//           text: 'Features',
-//           href: '#',
-//         },
-//         {
-//           text: 'Pricing',
-//           href: '#',
-//         },
-//         {
-//           text: 'About us',
-//           href: '#',
-//         },
-//         {
-//           text: 'Contact',
-//           href: '#',
-//         },
-//         {
-//           text: 'Terms',
-//           href: getPermalink('/terms'),
-//         },
-//         {
-//           text: 'Privacy policy',
-//           href: getPermalink('/privacy'),
-//         },
-//       ],
-//     },
-//     {
-//       text: 'Widgets',
-//       href: '#',
-//     },
-//     {
-//       text: 'Blog',
-//       href: getBlogPermalink(),
-//     },
-//   ],
-//   actions: [
-//     { type: 'button', text: 'Download', href: 'https://github.com/onwidget/astrowind' }
-//   ],
-// };
-
 export const headerData = {
   links: [
     {
@@ -82,15 +20,15 @@ export const headerData = {
       text: 'site.projects',
       href: ['projects', 'page'],
     },
-    // {
-    //   text: '/uses',
-    //   href: getBlogPermalink('uses', 'page'),
-    // },
+
     {
       text: 'site.about',
       href: ['about', 'page'],
     },
   ],
+  // actions: [
+  //     { type: 'button', text: 'Download', href: 'https://github.com/onwidget/astrowind' }
+  //   ],
 };
 
 export const footerData = {
